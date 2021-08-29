4a561b6c5996be226dcb275e9d4aeb4be32c773b
Adam Tilly <tillyjadam@gmail.com>
2021-08-28 22:54:35 -0500

    multiple rounds and ending complete

--- assets/js/game.js
+++ assets/js/game.js
@@ -9,8 +9,7 @@ console.log(playerName, playerAttack, playerHealth);
 var enemyNames = ["Roborto", "AMy Android", "Robo Trumble"];
 var enemyHealth = 50
 var enemyAttack = 12
-
-window.alert("Welcome to Robot Gladiators!")
+  window.alert("Welcome to Robot Gladiators!")
 
  var fight = function(enemyName){
    while(playerHealth > 0 && enemyHealth > 0){
@@ -64,6 +63,17 @@ window.alert("Welcome to Robot Gladiators!")
 //execute fuction
 
 for(var i = 0; i < enemyNames.length; i++){
+  if (playerHealth > 0){
+    window.alert( "Welcome to Robot Gladiators! Round " + (i + 1))
+    var pickedEnemyName = enemyNames[i]
+    enemyHealth = 50
+    //debugger
+  fight(pickedEnemyName)
+  }
+  else{
+    window.alert("You have lost your robot in battle! Game Over!")
+    break
+  }
   var pickedEnemyName = enemyNames[i]
   enemyHealth = 50
   fight(pickedEnemyName)
